51a48e0f8f724eb5dac556f737977597b826496f
Ingvar Sigurðssong <gvarihendrix@gmail.com>
2016-12-20 17:32:24 +0000

    Some modifications

--- server.js
+++ server.js
@@ -9,19 +9,32 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const infoRouter = require('./info-router');
+app.use((req, res, next) => {
+  if (req.baseUrl === '/api') {
+    res.type('json');
+  }
+  next();
+});
 
-app.use('/api/info-schema', infoRouter);
+app.use('/api/info-schema', require('./info-router'));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.status(200).end(JSON.stringify({ message: 'hello world' }));
 });
 
-
 app.get('/api', (req, res) => {
-  res.status(200).end(JSON.stringify(app._router.stack));
+  var url = `${req.protocol}://${req.hostname}/${req.baseUrl}`;
+  var m  = app._router.stack
+    .filter(r => r.path && r.path !== '')
+    .map(r => {
+      var p = r.path.substring(1, r.path.length);
+      return url + p;
+    });
+
+
+  res.status(200).end(JSON.stringify({ data: m }));
 });
 
 app.listen(PORT, () => {
